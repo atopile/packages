c9712777035a749daac7d74fa2fbaf39983fd4b7
Narayan Powderly <narayan@atopile.io>
2025-05-15 17:38:50 -0700

    ISO1640 initial commit

---
+++ packages/ti-iso1640x/components/i2c_isolator.py
@@ -0,0 +1,27 @@
+import faebryk.library._F as F
+from faebryk.core.module import Module
+from faebryk.libs.util import times
+from faebryk.libs.library import L
+
+
+class I2CIsolator(Module):
+    """
+    Connects two I2C interfaces together, without connecting the electricals below
+    Useful for passing logical connections through isolators/switches etc.
+    """
+
+    @L.rt_field
+    def power_rails(self) -> list[F.ElectricPower]:
+        return times(2, F.ElectricPower)
+
+    @L.rt_field
+    def i2cs(self) -> list[F.I2C]:
+        return times(2, F.I2C)
+
+    def __preinit__(self) -> None:
+        self.i2cs[0].connect_shallow(self.i2cs[1])
+
+
+class TwoIsolators(Module):
+    isolator1: I2CIsolator
+    isolator2: I2CIsolator
