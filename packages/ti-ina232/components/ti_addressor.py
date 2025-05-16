fcc798d3ba912879309160e890c6465eb990d58b
Narayan Powderly <narayan@atopile.io>
2025-05-16 13:55:50 -0700

    fix INA current sensor

--- packages/ti-ina232/components/ti_addressor.py
+++ packages/ti-ina232/components/ti_addressor.py
@@ -5,20 +5,17 @@ import logging
 import faebryk.library._F as F
 from faebryk.core.moduleinterface import ModuleInterface
 from faebryk.libs.library import L
-from faebryk.libs.util import times
 
 logger = logging.getLogger(__name__)
 
 
-class TI_I2C_Addressor(ModuleInterface):
+class TIAddressor(ModuleInterface):
     address = L.p_field(domain=L.Domains.Numbers.NATURAL())
     offset = L.p_field(domain=L.Domains.Numbers.NATURAL())
     base = L.p_field(domain=L.Domains.Numbers.NATURAL())
-
-    # Interfaces
+    num_addresses = L.p_field(domain=L.Domains.Numbers.NATURAL())
     address_line: F.ElectricLogic
     i2c: F.I2C
-    power: F.ElectricPower
 
     @L.rt_field
     def single_electric_reference(self):
@@ -32,13 +29,14 @@ class TI_I2C_Addressor(ModuleInterface):
 
         self.address.alias_is(self.base + self.offset)
 
-        if self.offset == 0:
-            self.address_line.line.connect(self.power.gnd)
-        elif self.offset == 1:
-            self.address_line.line.connect(self.power.vcc)
-        elif self.offset == 2:
-            self.address_line.line.connect(self.i2c.sda)
-        elif self.offset == 3:
-            self.address_line.line.connect(self.i2c.scl)
-        else:
-            raise ValueError("Offset must be 0, 1, 2 or 3")
+        for i, dest in enumerate(
+            [
+                self.address_line.reference.gnd,
+                self.address_line.reference.vcc,
+                self.i2c.sda.line,
+                self.i2c.scl.line,
+            ]
+        ):
+            (self.offset.operation_is_subset(i)).if_then_else(
+                lambda dest=dest: self.address_line.line.connect(dest)
+            )
