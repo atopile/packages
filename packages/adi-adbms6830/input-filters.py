7441434a26148edfefc7078e7deb377e6f8b709b
Nicholas Krstevski <nick@atopile.io>
2025-07-24 18:21:25 -0700

    picking and working

--- packages/adi-adbms6830/input-filters.py
+++ packages/adi-adbms6830/input-filters.py
@@ -1,9 +1,13 @@
+import math
+from faebryk.core.parameter import Quantity_Interval
+from faebryk.library.FilterElectricalLC import FilterElectricalLC
 import faebryk.library._F as F  # noqa: F401
 from faebryk.libs.units import P, dimensionless  # noqa: F401
 from faebryk.core.module import Module  # noqa: F401
 from faebryk.libs.library import L  # noqa: F401
 from faebryk.libs.util import times  # noqa: F401
 from faebryk.libs.units import Quantity
+from faebryk.libs.sets.quantity_sets import Quantity_Interval_Disjoint
 
 
 class ADBMS6830InputFilters(Module):
@@ -22,59 +26,44 @@ class ADBMS6830InputFilters(Module):
 
     def __init__(
         self,
-        number_of_cells: int = 16,
-        sense_filter_resistance: Quantity = 200 * P.Ω,
-        sense_filter_capacitance: Quantity = 10 * P.nF,
-        max_balance_current: Quantity = 0.100 * P.A,
+        number_of_cells: int = 6,
+        sense_filter_resistance_ohms: int = 200,
+        sense_filter_capacitance_nF: int = 10,
+        max_balance_current_mA: float = 200,
         total_number_of_channels: int = 16,
     ):
         super().__init__()
         self.number_of_cells = number_of_cells
-        self.sense_filter_resistance = sense_filter_resistance
-        self.sense_filter_capacitance = sense_filter_capacitance
-        self.max_balance_current = max_balance_current
+        self.sense_filter_resistance = L.Range.from_center_rel(
+            sense_filter_resistance_ohms * P.Ω, 1 * P.percent
+        )
+        self.sense_filter_capacitance = L.Range.from_center_rel(
+            sense_filter_capacitance_nF * P.nF, 20 * P.percent
+        )
+        self.max_balance_current = max_balance_current_mA * P.mA
         self.total_number_of_channels = total_number_of_channels
-
-    bleed_resistance = L.p_field(
-        units=P.Ω
-    )  # , likely_constrained=True, soft_set=L.Range(10 * P.Ω, 100 * P.Ω), tolerance_guess=1 * P.percent)
-
-    @L.rt_field
-    def cell_inputs(self) -> list[F.ElectricPower]:
-        return times(self.number_of_cells, F.ElectricPower)
-
-    @L.rt_field
-    def sense_inputs(self) -> list[F.DifferentialPair]:
-        return times(self.number_of_cells, F.DifferentialPair)
-
-    @L.rt_field
-    def balance_inputs(self) -> list[F.DifferentialPair]:
-        return times(self.number_of_cells, F.DifferentialPair)
 
     class SenseFilter(Module):
         def __init__(
             self,
-            number_of_cells: int = 16,
-            sense_filter_resistance: Quantity = 200 * P.Ω,
-            sense_filter_capacitance: Quantity = 10 * P.nF,
+            sense_filter_resistance: Quantity_Interval,
+            sense_filter_capacitance: Quantity_Interval,
         ):
             super().__init__()
-            self.number_of_cells = number_of_cells
             self.sense_filter_resistance = sense_filter_resistance
             self.sense_filter_capacitance = sense_filter_capacitance
 
         cell_power: F.ElectricPower
         sense_input: F.DifferentialPair
 
-        sense_filter: F.FilterElectricalRC
-
-        def __preinit__(self):
-            # Set value of sense filter
-            self.sense_filter.resistor.resistance.alias_is(self.sense_filter_resistance)
-            self.sense_filter.capacitor.capacitance.alias_is(
-                self.sense_filter_capacitance
+        @L.rt_field
+        def sense_filter(self):
+            return F.FilterElectricalRC.hardcoded_rc(
+                self.sense_filter_resistance,
+                self.sense_filter_capacitance,
             )
 
+        def __preinit__(self):
             self.cell_power.hv.connect_via(
                 self.sense_filter.resistor, self.sense_input.p.line
             )
@@ -85,34 +74,42 @@ class ADBMS6830InputFilters(Module):
     class BalanceFilter(Module):
         def __init__(
             self,
-            number_of_cells: int = 16,
-            balance_filter_resistance: Quantity = 15 * P.Ω,  # TODO: Calculate these
-            balance_filter_capacitance: Quantity = 100 * P.nF,
+            bleed_resistance_target: float,
+            bleed_filter_capacitance: Quantity_Interval,
         ):
             super().__init__()
-            self.number_of_cells = number_of_cells
-            self.balance_filter_resistance = balance_filter_resistance
-            self.balance_filter_capacitance = balance_filter_capacitance
+            self.bleed_resistance_target = bleed_resistance_target
+            self.bleed_filter_capacitance = bleed_filter_capacitance
 
         cell_power: F.ElectricPower
         balance_input: F.DifferentialPair
         cap_bridge_connect: F.ElectricSignal  # Connect to cell below, transients to gnd
 
-        top_balance_filter: F.FilterElectricalRC
-        bottom_balance_filter: F.FilterElectricalRC
+        @L.rt_field
+        def top_balance_filter(self):
+            bleed_resistance = L.Range.from_center_rel(
+                self.bleed_resistance_target,
+                5 * P.percent,
+            )
+            return F.FilterElectricalRC.hardcoded_rc(
+                bleed_resistance,
+                self.bleed_filter_capacitance,
+            )
+
+        @L.rt_field
+        def bottom_balance_filter(self):
+            bleed_resistance = L.Range.from_center_rel(
+                self.bleed_resistance_target,
+                5 * P.percent,
+            )
+            return F.FilterElectricalRC.hardcoded_rc(
+                bleed_resistance,
+                self.bleed_filter_capacitance,
+            )
 
         def __preinit__(self):
-            self.top_balance_filter.resistor.resistance.alias_is(
-                self.balance_filter_resistance
-            )
-            self.bottom_balance_filter.resistor.resistance.alias_is(
-                self.balance_filter_resistance
-            )
-            self.top_balance_filter.capacitor.capacitance.alias_is(
-                self.balance_filter_capacitance
-            )
-            self.bottom_balance_filter.capacitor.capacitance.alias_is(
-                self.balance_filter_capacitance
+            self.top_balance_filter.resistor.max_power.constrain_ge(
+                (4.25 / 2 * P.V) * (4.25 / 2 * P.V) / (self.bleed_resistance_target)
             )
             self.cell_power.hv.connect_via(
                 self.top_balance_filter.resistor, self.balance_input.p.line
@@ -128,24 +125,65 @@ class ADBMS6830InputFilters(Module):
             )
 
     @L.rt_field
+    def cell_inputs(self) -> list[F.ElectricPower]:
+        return times(self.number_of_cells, F.ElectricPower)
+
+    @L.rt_field
+    def sense_inputs(self) -> list[F.DifferentialPair]:
+        return times(self.total_number_of_channels, F.DifferentialPair)
+
+    @L.rt_field
+    def balance_inputs(self) -> list[F.DifferentialPair]:
+        return times(self.total_number_of_channels, F.DifferentialPair)
+
+    @L.rt_field
     def sense_filters(self) -> list[SenseFilter]:
-        return times(self.number_of_cells, self.SenseFilter)
+        return times(
+            self.number_of_cells,
+            lamb=lambda: self.SenseFilter(
+                self.sense_filter_resistance, self.sense_filter_capacitance
+            ),
+        )
 
     @L.rt_field
     def balance_filters(self) -> list[BalanceFilter]:
-        return times(self.number_of_cells, self.BalanceFilter)
-
-    bottom_sense_filter: SenseFilter
-
-    def __preinit__(self):
-        # Calculate balance resistance value based on max balance current
-        self.bleed_resistance.constrain_ge(
-            ((self.cell_inputs[0].voltage / self.max_balance_current) - 1.0 * P.ohm) / 2
+        bleed_resistance_target = ((4.25 * P.V) / self.max_balance_current) / 2
+        bleed_filter_capacitance = L.Range.from_center_rel(
+            1 / (bleed_resistance_target * (2 * math.pi) * (80 * P.kHz)),
+            20 * P.percent,
+        )
+        return times(
+            self.number_of_cells,
+            lamb=lambda: self.BalanceFilter(
+                bleed_resistance_target, bleed_filter_capacitance
+            ),
         )
 
-        # Stack all cell power interface in series
-        # Connect input filters
+    """
+    Bottom sense filter, used to connect the bottom of the first cell to the lowest
+    ADC channel - side and filter transients to gnd
+    """
+
+    def __preinit__(self):
+        # self.bleed_resistance_target.alias_is(
+        #     ((4.25 * P.V) / self.max_balance_current) / 2
+        # )
+        # Calculate balance resistance value based on max balance current
         for idx, cell_input in enumerate(self.cell_inputs):
+            # Set filter component values
+            # self.sense_filters[idx].sense_filter.hardcoded_rc(
+            #     L.Range.from_center_rel(10 * P.ohms, 5 * P.percent),
+            #     L.Range.from_center_rel(47 * P.nF, 20 * P.percent),
+            # )
+            # self.balance_filters[idx].top_balance_filter.hardcoded_rc(
+            #     L.Range.from_center_rel(10 * P.ohms, 5 * P.percent),
+            #     L.Range.from_center_rel(47 * P.nF, 20 * P.percent),
+            # )
+            # self.balance_filters[idx].bottom_balance_filter.hardcoded_rc(
+            #     L.Range.from_center_rel(10 * P.ohms, 5 * P.percent),
+            #     L.Range.from_center_rel(47 * P.nF, 20 * P.percent),
+            # )
+
             cell_input.connect(self.sense_filters[idx].cell_power)
             self.sense_filters[idx].sense_input.connect(self.sense_inputs[idx])
 
@@ -162,17 +200,25 @@ class ADBMS6830InputFilters(Module):
                 )
 
         # Connect Bottom of Stack to GND with additional sense filter
+        self.bottom_sense_filter = self.SenseFilter(
+            self.sense_filter_resistance, self.sense_filter_capacitance
+        )
         self.bottom_sense_filter.cell_power.hv.connect(self.cell_inputs[0].lv)
         self.bottom_sense_filter.sense_input.p.line.connect(self.sense_inputs[0].n.line)
         self.bottom_sense_filter.sense_input.n.line.connect(self.cell_inputs[0].lv)
 
-        # Depopulated channels
+        """
+        If there are any depopulated channels, connect both the sense and
+        balance inputs to the top of the stack through a 1kohm resistor
+        """
         number_of_depopulated_channels = (
             self.total_number_of_channels - self.number_of_cells
         )
         if number_of_depopulated_channels > 0:
             depop_resistor = self.add(F.Resistor())
-            depop_resistor.resistance.alias_is(1 * P.kΩ)
+            depop_resistor.resistance.constrain_subset(
+                L.Range.from_center_rel(1 * P.kΩ, 1 * P.percent)
+            )
             for idx in range(number_of_depopulated_channels):
                 self.sense_inputs[
                     self.total_number_of_channels - 1 - idx
