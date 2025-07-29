d7dfa06ec17e78210cbaf9da73b8bb35517d2f7a
Sam Mellor <sam@atopile.io>
2025-07-29 15:19:59 -0700

    update for built-in enum support

--- packages/mounting_holes/MountingHole.py
+++ packages/mounting_holes/MountingHole.py
@@ -2,11 +2,10 @@
 # SPDX-License-Identifier: MIT
 
 import logging
-from enum import StrEnum
+from enum import Enum, StrEnum
 from pathlib import Path
 
 import faebryk.library._F as F
-from atopile.errors import UserBadParameterError
 from faebryk.core.module import Module
 from faebryk.libs.iso_metric_screw_thread import Iso262_MetricScrewThreadSizes
 from faebryk.libs.library import L
@@ -23,6 +22,16 @@ class MountingHole(Module):
         Pad_TopOnly = "Pad_TopOnly"
         Pad_Via = "Pad_Via"
 
+    # We currently only have footprints for these sizes
+    class SupportedMetricScrewSizes(Enum):
+        M2 = Iso262_MetricScrewThreadSizes.M2.value
+        M2_5 = Iso262_MetricScrewThreadSizes.M2_5.value
+        M3 = Iso262_MetricScrewThreadSizes.M3.value
+        M4 = Iso262_MetricScrewThreadSizes.M4.value
+        M5 = Iso262_MetricScrewThreadSizes.M5.value
+        M6 = Iso262_MetricScrewThreadSizes.M6.value
+        M8 = Iso262_MetricScrewThreadSizes.M8.value
+
     contact: F.Electrical
 
     attach_to_footprint: F.can_attach_to_footprint_symmetrically
@@ -32,21 +41,6 @@ class MountingHole(Module):
 
     def footprint_name(self) -> str:
         # e.g. MountingHole_2.7mm_M2.5_Pad_TopOnly
-        supported_metric_sizes = [
-            Iso262_MetricScrewThreadSizes.M2,
-            Iso262_MetricScrewThreadSizes.M2_5,
-            Iso262_MetricScrewThreadSizes.M3,
-            Iso262_MetricScrewThreadSizes.M4,
-            Iso262_MetricScrewThreadSizes.M5,
-            Iso262_MetricScrewThreadSizes.M6,
-            Iso262_MetricScrewThreadSizes.M8,
-        ]
-        # We currently only have footprints for these sizes
-        if self._metric_screw_size not in supported_metric_sizes:
-            raise UserBadParameterError(
-                f"Unsupported metric_screw_size: [{self._metric_screw_size.name}]. "
-                f"Supported sizes are: {[m.name for m in supported_metric_sizes]}."
-            )
         size_metric = self._metric_screw_size
         size_mm = f"{size_metric.value}mm"
         padtype_name = self._pad_type.value
@@ -55,14 +49,8 @@ class MountingHole(Module):
 
         return f"MountingHole_{size_mm}_{size_metric.name}{padtype_name}"
 
-    def __init__(
-        self, metric_screw_size: Iso262_MetricScrewThreadSizes, pad_type: PadType
-    ):
+    def __init__(self, metric_screw_size: SupportedMetricScrewSizes, pad_type: PadType):
         super().__init__()
-        if isinstance(metric_screw_size, str):
-            metric_screw_size = Iso262_MetricScrewThreadSizes[metric_screw_size]
-        if isinstance(pad_type, str):
-            pad_type = self.PadType[pad_type]
         self._metric_screw_size = metric_screw_size
         self._pad_type = pad_type
 
